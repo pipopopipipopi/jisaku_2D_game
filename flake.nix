983d518802f74935f72a010ab7893d0d2ed8fd12
pipopo <puchi.maru.milk@gmail.com>
2025-02-11 09:57:22 +0900

    first

---
+++ flake.nix
@@ -0,0 +1,64 @@
+{
+  description = "SDL2 rust";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      perSystem = { pkgs, system, ... }: {
+        _module.args.pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [
+            inputs.fenix.overlays.default
+          ];
+        };
+
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "jisaku_2D_game";
+          version = "0.1.0";
+
+          src = ./.;
+
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+
+          nativeBuidInputs = with pkgs; [
+            fenix.stable.rustc
+            fenix.stable.cargo
+          ];
+
+          buildInputs = with pkgs; [
+            SDL2
+            SDL2_image
+          ];
+        };
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            (fenix.stable.withComponents [
+              "rustc"
+              "cargo"
+              "rust-analyzer"
+            ])
+            SDL2
+            SDL2_image
+          ];
+          shellHook = ''
+            exec fish
+          '';
+        };
+      };
+    };
+}
