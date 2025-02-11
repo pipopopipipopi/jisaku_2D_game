4e4a6d8bfb678c930260b2858c28af73ab7d743c
pipopo <puchi.maru.milk@gmail.com>
2025-02-11 11:44:08 +0900

    ウィンドウ、フィールドの作成

---
+++ src/constants.rs
@@ -0,0 +1,6 @@
+pub const FIELD_WIDTH: u32 = 9;
+pub const FIELD_HEIGHT: u32 = 4;
+pub const TILE_SIZE: u32 = 64;
+pub const TOP_MARGIN: u32 = 5;
+pub const BOTTOM_MARGIN: u32 = 1;
+pub const SIDE_MARGIN: u32 = 3;
