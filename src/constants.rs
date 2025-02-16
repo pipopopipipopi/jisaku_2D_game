dfa57d91b89e482349280c52d3d299bafd26fc61
pipopo <puchi.maru.milk@gmail.com>
2025-02-16 22:33:46 +0900

    tile size 64 -> 100

--- src/constants.rs
+++ src/constants.rs
@@ -1,6 +1,6 @@
 pub const FIELD_WIDTH: u32 = 9;
 pub const FIELD_HEIGHT: u32 = 4;
-pub const TILE_SIZE: u32 = 64;
+pub const TILE_SIZE: u32 = 100;
 pub const TOP_MARGIN: u32 = 3;
 pub const BOTTOM_MARGIN: u32 = 0;
 pub const SIDE_MARGIN: u32 = 1;
